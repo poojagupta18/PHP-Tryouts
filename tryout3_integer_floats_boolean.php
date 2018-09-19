04ec3aa9d55dac8acac082a3a5834b5e7b89bcae
poojagupta18 <pujagupta5377@gmail.com>
2018-09-19 21:41:22 +0530

    About Integer, float and boolean datatype

---
+++ tryout3_integer_floats_boolean.php
@@ -0,0 +1,18 @@
+<?php
+
+//to perform arithmetic calculations floats and integers are used 
+//php is loosley tied language.
+
+ $pi = 3.141;          //float value assignment
+// echo pi * 5;
+$bankBalance = 1.2e6;  //1.2e6 denotes 1.2 millions
+
+echo 'Seema\'s bank balanace is '.$bankBalance;
+
+$checkVar = true;
+
+if($checkVar == 1)
+{
+	var_dump($checkVar);
+}
+?>
